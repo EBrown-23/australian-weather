3e12cd12f72a54e17c4b5d577fee193b7b40e215
EBrown-23 <124892564+EBrown-23@users.noreply.github.com>
2023-02-14 11:58:03 +0000

    Changed the scatterplot points to green

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,7 +15,7 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(colour = "red") +
+  geom_point(colour = "green") +
   labs(title = "Temperature and humidity at 9am in Brisbane") +
   theme_minimal() + 
   facet_wrap(~WindDir9am)
